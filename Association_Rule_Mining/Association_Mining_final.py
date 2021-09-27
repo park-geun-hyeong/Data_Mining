ff6a27e11b80e581ab86e406e2207348bd49e16e
park <park30908@gmail.com>
2021-09-27 15:54:18 +0900

    modify fianl

--- Association_Rule_Mining/Association_Mining_final.py
+++ Association_Rule_Mining/Association_Mining_final.py
@@ -1,20 +1,40 @@
 from collections import defaultdict
+from itertools import combinations
 import time
 import csv
 
 
 def confidence(a, b, frequent_item_dict):
+    '''
+    Calculation confidence between item A and B
+
+    :param a: name of item A
+    :param b: name of item B
+    :param frequent_item_dict: supports(dict) of frequent_items
+    :return: confidence of item A ==> B & confidence of item B ==> A
+    '''
+
     support_a = frequent_item_dict[a]
+    support_b = frequent_item_dict[b]
     union = 0
 
     for i in data:
         if (a in i) and (b in i):
             union += 1
 
-    return union / support_a
+    return [union / support_a, union / support_b]
 
 
-def association_rule_mining(data: list, support_th=10, confidence_th=0.1):
+def association_rule_mining(data: list, support_th=100, confidence_th=0.2):
+    '''
+    AFriori Algorithm for association rule mining
+
+    :param data: list of transaction
+    :param support_th: support threshold(default : 100)
+    :param confidence_th: confidence threshold(default : 0.2)
+    :return: sorted list of Association rule's confidence & interest
+    '''
+
     def zero():
         return int(0)
 
@@ -31,26 +51,19 @@ def association_rule_mining(data: list, support_th=10, confidence_th=0.1):
             frequent_item_dict[i[0]] = i[1]
 
     item_key = [i for i in frequent_item_dict.keys()]
-
-    combination = []
-    for i in range(len(item_key)):
-        for j in range(len(item_key)):
-            if i == j:
-                continue
-            else:
-                combination.append((item_key[i], item_key[j]))
+    combination = list(combinations(item_key, 2))
 
     ## 2 pass
     confidence_dict = {}
     for i in combination:
-        if confidence(i[0], i[1], frequent_item_dict) > confidence_th:
-            confidence_dict[i] = confidence(i[0], i[1], frequent_item_dict)
+        confidence_dict[(i[0], i[1])] = confidence(i[0], i[1], frequent_item_dict)[0]
+        confidence_dict[(i[1], i[0])] = confidence(i[0], i[1], frequent_item_dict)[1]
 
-    interest_dict = dict()
-    for i in confidence_dict.items():
-        interest_dict[f"{i[0][0]} ==> {i[0][1]}"] = round((i[1] - frequent_item_dict[i[0][1]] / len(data)), 6)
+    include_interest = [
+        [f"{i[0][0]} ==> {i[0][1]}", round(i[1], 6), round((i[1] - frequent_item_dict[i[0][1]] / len(data)), 6)]
+         for i in sorted(confidence_dict.items(), key=lambda x: x[1], reverse=True) if i[1] > confidence_th]
 
-    return sorted(interest_dict.items(), key=lambda x: x[1], reverse=True)
+    return include_interest
 
 
 if __name__ == '__main__':
@@ -66,6 +79,7 @@ if __name__ == '__main__':
 
     f.close()
 
-    print("====== Interest of association rule =====")
-    print(association_rule_mining(data)[:100])
+    print("====== Confidence & Interest of association rule =====\n")
+    for i in association_rule_mining(data):
+        print(i)
     print(f"Time : {time.time() - start :.4f} sec, Num of itemSet : {len(association_rule_mining(data))}")
